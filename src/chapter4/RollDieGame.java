b6714fc78b0a955738cd460be992fb2890e18607
Jihan Yasin <jyasin@dvt.co.za>
2022-06-15 13:51:19 +0200

    Second commit

--- src/chapter4/RollDieGame.java
+++ src/chapter4/RollDieGame.java
@@ -4,28 +4,43 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RollDieGame {
+
+
     public static void main(String arg[])
     {
-        boolean again = false;
+
         //Initialize the known
         System.out.println("Roll the dice");
         Random random = new Random(); //Obj calling method
         int total = 20;
-        int space;
+        //int space;
 
 
         for(int j=1; j<6; j++)
         {
             int die = random.nextInt(6) + 1;
             total = total- die;
-            space = 20-total;
+           int space = 20-total;
 
+           if(j<5)
+           {
+               System.out.println("Roll #" + j + ": You've rolled a " + die + ". You are now on space " + space + " and have "
+                       + total + " more to go." );
+           }
 
-            System.out.println("Roll #" + j + ": You've rolled a " + die + " You at " + total + " You at " + space);
+            if (space == 20 && j == 5)
+            {
+                System.out.println("Roll #" + j + ": You've rolled a " + die + ". Congrats! You win");
+            }
+            else if (space != 20 && j == 5)
+            {
+                System.out.println("Roll #" + j + ": You've rolled a " + die + ". You LOOOOSE" );
 
+            }
 
 
         }
+
 
 
 
